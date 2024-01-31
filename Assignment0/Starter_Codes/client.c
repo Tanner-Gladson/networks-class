5877d306029fd03c3213eb4b5a1e93769769d7a2
Tanner Gladson <tannergladson@gmail.com>
2024-01-31 12:16:20 -0500

    I think it works

--- Assignment0/Starter_Codes/client.c
+++ Assignment0/Starter_Codes/client.c
@@ -17,7 +17,65 @@
  * Return 0 on success, non-zero on failure
 */
 int client(char *server_ip, char *server_port) {
-    return 0;
+  // Must "gracefully handle all errors returned by socket API". If the
+  // function might set errno, call perror(). If the function does not set
+  // errno, you can just print an error to std::err. "make sure to flush
+  // stdout using fflush(stdout)"
+
+  // Attempt to open a socket
+  struct addrinfo hints, *serverinfo, *p;
+
+  memset(&hints, 0, sizeof hints); 
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+
+  // Prepare addrinfo so we can send data to server
+  int err = getaddrinfo(server_ip, server_port, &hints, &serverinfo);
+  if (err != 0) {
+    fprintf(stderr, "Error getting addr info");
+    return EXIT_FAILURE;
+  }
+
+  // Remark that the socket is IP agnostic?
+  int sock_fd = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
+  if (sock_fd == -1) {
+    perror("Error opening socket fd");
+    return EXIT_FAILURE;
+  }
+
+  // Do we need to pass addrlen so that it can handle both IPv4 and IPv6?
+  err = connect(sock_fd, serverinfo->ai_addr, serverinfo->ai_addrlen);
+  if (err == -1) {
+    perror("Error connecting to server");
+    return EXIT_FAILURE;
+  }
+
+  // Read a message from stdin
+  char msg[SEND_BUFFER_SIZE];
+  int msg_len = read(STDIN_FILENO, msg, SEND_BUFFER_SIZE);
+  if (msg_len == -1) {
+    perror("Error reading message");
+    return EXIT_FAILURE;
+  }
+  
+  // Send the message. Sometimes we can't do it all at once
+  int bytes_remaining = msg_len;
+  while (bytes_remaining > 0) {
+    int start_char = msg_len - bytes_remaining;
+    
+    int bytes_sent = send(sock_fd, msg, start_char, 0);
+    if (bytes_sent == -1) {
+      perror("Error sending message");
+      return EXIT_FAILURE;
+    }
+
+    bytes_remaining -= bytes_sent;
+  }
+
+  close(sock_fd);
+  freeaddrinfo(serverinfo); // free the linked-list
+  fflush(stdout);
+  return 0;
 }
 
 /*
--- Assignment0/Starter_Codes/server.c
+++ Assignment0/Starter_Codes/server.c
@@ -18,7 +18,84 @@
  * Return 0 on success, non-zero on failure
 */
 int server(char *server_port) {
-    return 0;
+  // First we need to store this server's TCP/IP info 
+  struct addrinfo hints, *serverinfo, *p;
+  struct sockaddr_storage client_addr;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE;      // Use host IP
+
+  int err = getaddrinfo(NULL, server_port, &hints, &serverinfo);
+  if (err != 0) {
+    fprintf(stderr, "Error getting addr info");
+    return EXIT_FAILURE;
+  }
+
+  // loop through all the results and bind to the first we can
+  int yes = 1;
+  int sock_fd;
+  for(p = serverinfo; p != NULL; p = p->ai_next) {
+    if ((sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("Error opening socket");
+      continue;
+    }
+
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("Error setting socket options");
+      return EXIT_FAILURE;
+    }
+
+    if (bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sock_fd);
+      perror("Error binding socket");
+      continue;
+    }
+    break;
+  }
+  freeaddrinfo(serverinfo);
+  if (p == NULL)  {
+    fprintf(stderr, "server: failed to bind\n");
+    return EXIT_FAILURE;
+  }
+
+  if (listen(sock_fd, QUEUE_LENGTH) == -1) {
+    perror("Could not listen");
+    return EXIT_FAILURE;
+  }
+
+  // Clients can connect whenever, we'll be listening
+  char rx_buffer[RECV_BUFFER_SIZE];
+  while (1) {
+    // We must accept connections
+    socklen_t sin_size = sizeof client_addr;
+    int client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &sin_size);
+    if (client_fd == -1) {
+        perror("accept");
+        continue;
+    }
+
+    // TODO: is this necessary?
+    // char s[INET6_ADDRSTRLEN];
+    // inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *)&client_addr), s, sizeof s);
+
+    // Connection successful, so we can read off data
+    int bytes_rx = recv(client_fd, rx_buffer, RECV_BUFFER_SIZE-1, 0);
+    if (bytes_rx == -1) {
+      perror("Error reading message");
+      continue;
+    } else if (bytes_rx == 0) {
+      printf("Client closed connection");
+    }
+
+    fwrite(rx_buffer, sizeof rx_buffer[0], bytes_rx, stdout);
+    err = close(client_fd);
+    if (err == -1) {
+      perror("Error closing client connection");
+      continue;
+    }
+  }
 }
 
 /*

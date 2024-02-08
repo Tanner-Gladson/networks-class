4fe5bf336e678d61a7f2aff660b6ad2f28a8d683
Tanner Gladson <tannergladson@gmail.com>
2024-02-07 22:02:28 -0500

    moved boilerplate to other function

--- Assignment1/starter_code/proxy.c
+++ Assignment1/starter_code/proxy.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <errno.h>
@@ -15,8 +16,16 @@
 #define MAX_NUMBER_FORKS 100
 #define MAX_BUFFER_SIZE 2048
 
-// Local function declarations
-int proxy(char *proxy_port);
+// Local function prototypes
+int start_proxy(char *proxy_port);
+int handle_connection(int client_fd);
+int _attempt_handle_connection(int client_fd, struct ParsedRequest* request);
+int read_http_request(int client_fd, struct ParsedRequest* request);
+int forward_http_request(struct ParsedRequest* request, char* response_buffer, int buffer_len);
+int open_proxy_listening_socket(char *proxy_port);
+int open_server_socket(char *server_ip, char *server_port);
+int send_text(int client_fd, char *data, int length);
+
 
 int main(int argc, char *argv[])
 {
@@ -29,82 +38,43 @@ int main(int argc, char *argv[])
   }
 
   proxy_port = argv[1];
-  return proxy(proxy_port);
+  return start_proxy(proxy_port);
 }
 
-/* TODO: proxy()
- * Establish a socket connection to listen for incoming connections.
- * Accept each client request in a new process.
- * Parse header of request and get requested URL.
- * Get data from requested remote server.
- * Send data to the client
- * Return 0 on success, non-zero on failure
- */
-int proxy(char *proxy_port)
+
+int start_proxy(char *proxy_port)
 {
-  // First we need to store this server's TCP/IP info
-  struct addrinfo hints;
-  struct addrinfo *serverinfo;
-  struct addrinfo *p;
-
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
-  hints.ai_flags = AI_PASSIVE;     // Use host IP
-
-  int err = getaddrinfo(NULL, proxy_port, &hints, &serverinfo);
-  if (err != 0)
-  {
-    fprintf(stderr, "Error getting addr info");
-    return -1;
-  }
-
-  // Bind our socket to the port
-  int yes = 1;
-  int sock_fd;
-  for (p = serverinfo; p != NULL; p = p->ai_next)
-  {
-    if ((sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-    {
-      perror("Error opening socket");
-      continue;
-    }
-
-    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-    {
-      perror("Error setting socket options");
-      return -1;
-    }
-
-    if (bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1)
-    {
-      close(sock_fd);
-      perror("Error binding socket");
-      continue;
-    }
-    break;
-  }
-  freeaddrinfo(serverinfo);
-  if (p == NULL)
-  {
-    fprintf(stderr, "server: failed to bind\n");
-    return -1;
-  }
-
+  int sock_fd = open_proxy_listening_socket(proxy_port);
   if (listen(sock_fd, LISTEN_QUEUE_LEN) == -1)
   {
     perror("Could not listen");
     return -1;
   }
+
   #ifdef DEBUG
     printf("Bound to socket. Waiting for connections...\n");
   #endif
 
+
   // Clients can connect whenever, we'll be listening
   struct sockaddr_storage client_addr;
   socklen_t sin_size = sizeof client_addr;
+  int num_active_forks = 0;
+
+  // TODO: do I need to add a signal handler?
   while (1)
   {
+    // Reap zombies
+    while (waitpid(-1, NULL, WNOHANG) > 0) {
+      num_active_forks--;
+    }
+
+    // Limit number of forks, block until one dies
+    while (num_active_forks >= MAX_NUMBER_FORKS) {
+      waitpid(-1, NULL, 0);
+      num_active_forks--;
+    }
+    
     // We must accept connections
     int client_fd = accept(sock_fd, (struct sockaddr *) &client_addr, &sin_size);
     if (client_fd == -1)
@@ -113,30 +83,37 @@ int proxy(char *proxy_port)
       continue;
     }
 
-    #ifdef DEBUG
+    #ifdef DEBUG // Debug with a single process, much easier
       printf("Accepted message, forking a child\n");
+      handle_connection(client_fd);
+    #else
+      // TODO: limit number of forks
+      if (!fork())
+      { 
+        // entire child process
+        close(sock_fd);
+        handle_connection(client_fd, &client_addr);
+        close(client_fd);
+        exit(0);
+      }
+      num_active_forks++;
     #endif
-
-    // TODO: limit number of forks
-    if (!fork())
-    { 
-      // entire child process
-      close(sock_fd);
-      handle_connection(client_fd, &client_addr);
-      close(client_fd);
-      exit(0);
-    }
     close(client_fd);
   }
   
-  // TODO: do I need to wait on all child processes?
+  // Reap zombies, blocking
+  while (num_active_forks > 0) {
+    waitpid(-1, NULL, 0);
+    num_active_forks--;
+  }
   return 0;
 }
+
 
 int handle_connection(int client_fd)
 {
   struct ParsedRequest* request_buffer = ParsedRequest_create();
-  int status = _try_handle_connection(client_fd, request_buffer);
+  int status = _attempt_handle_connection(client_fd, request_buffer);
 
   // Clean up the connection
   ParsedRequest_destroy(request_buffer);
@@ -147,7 +124,8 @@ int handle_connection(int client_fd)
   return status;
 }
 
-int _try_handle_connection(int client_fd, struct ParsedRequest* request) {
+
+int _attempt_handle_connection(int client_fd, struct ParsedRequest* request) {
   /* Get client's requests */
   int err = read_http_request(client_fd, request);
   if (err == -501) {
@@ -166,7 +144,7 @@ int _try_handle_connection(int client_fd, struct ParsedRequest* request) {
 
   /* Forward request to server */
   char response[MAX_BUFFER_SIZE];
-  int response_len = forward_http_request(request, response);
+  int response_len = forward_http_request(request, response, sizeof response);
   if (err == -400) {
     send(client_fd, "HTTP/1.0 400 Bad Request\r\n\r\n", 26, 0);
     return -1;
@@ -174,8 +152,8 @@ int _try_handle_connection(int client_fd, struct ParsedRequest* request) {
     return -1;
   }
 
-  /* Send back to the client */
-  send_text(client_fd, response, response_len);
+  /* Send server's response back to the client */
+  return send_text(client_fd, response, response_len);
 }
 
 
@@ -198,7 +176,7 @@ int read_http_request(int client_fd, struct ParsedRequest* request) {
   return 0;
 }
 
-// Return the number of bytes that the server responded with
+
 int forward_http_request(struct ParsedRequest* request, char* response_buffer, int buffer_len) {
   // If hostname does not have a port, use 80
   char port[6];
@@ -258,6 +236,60 @@ int forward_http_request(struct ParsedRequest* request, char* response_buffer, i
   return bytes_rx;
 }
 
+
+int open_proxy_listening_socket(char *proxy_port) {
+   // First we need to store this server's TCP/IP info
+  struct addrinfo hints;
+  struct addrinfo *serverinfo;
+  struct addrinfo *p;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE;     // Use host IP
+
+  int err = getaddrinfo(NULL, proxy_port, &hints, &serverinfo);
+  if (err != 0)
+  {
+    fprintf(stderr, "Error getting addr info");
+    return -1;
+  }
+
+  // Bind our socket to the port
+  int yes = 1;
+  int sock_fd;
+  for (p = serverinfo; p != NULL; p = p->ai_next)
+  {
+    if ((sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      perror("Error opening socket");
+      continue;
+    }
+
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    {
+      perror("Error setting socket options");
+      return -1;
+    }
+
+    if (bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sock_fd);
+      perror("Error binding socket");
+      continue;
+    }
+    break;
+  }
+  freeaddrinfo(serverinfo);
+  if (p == NULL)
+  {
+    fprintf(stderr, "server: failed to bind\n");
+    return -1;
+  }
+  return sock_fd;
+}
+
+
 int open_server_socket(char *server_ip, char *server_port) {
   struct addrinfo hints, *serverinfo, *p;
   memset(&hints, 0, sizeof hints);
@@ -296,6 +328,7 @@ int open_server_socket(char *server_ip, char *server_port) {
   }
   return sock_fd;
 }
+
 
 int send_text(int client_fd, char *data, int length) {
   int bytes_remaining = length;

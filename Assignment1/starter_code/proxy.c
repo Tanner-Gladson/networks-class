ef296a232ac3642c9d4632acbad65962a5f831af
Tanner Gladson <tannergladson@gmail.com>
2024-02-07 21:20:51 -0500

    Ready to start debugging

--- Assignment1/starter_code/proxy.c
+++ Assignment1/starter_code/proxy.c
@@ -15,11 +15,8 @@
 #define MAX_NUMBER_FORKS 100
 #define MAX_BUFFER_SIZE 2048
 
+// Local function declarations
 int proxy(char *proxy_port);
-int handle_connection();
-void read_http_request();
-void forward_http_request();
-void return_http_request();
 
 int main(int argc, char *argv[])
 {
@@ -59,7 +56,7 @@ int proxy(char *proxy_port)
   if (err != 0)
   {
     fprintf(stderr, "Error getting addr info");
-    return EXIT_FAILURE;
+    return -1;
   }
 
   // Bind our socket to the port
@@ -76,7 +73,7 @@ int proxy(char *proxy_port)
     if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
     {
       perror("Error setting socket options");
-      return EXIT_FAILURE;
+      return -1;
     }
 
     if (bind(sock_fd, p->ai_addr, p->ai_addrlen) == -1)
@@ -91,15 +88,17 @@ int proxy(char *proxy_port)
   if (p == NULL)
   {
     fprintf(stderr, "server: failed to bind\n");
-    return EXIT_FAILURE;
+    return -1;
   }
 
   if (listen(sock_fd, LISTEN_QUEUE_LEN) == -1)
   {
     perror("Could not listen");
-    return EXIT_FAILURE;
+    return -1;
   }
-  printf("Bound to socket. Waiting for connections...\n");
+  #ifdef DEBUG
+    printf("Bound to socket. Waiting for connections...\n");
+  #endif
 
   // Clients can connect whenever, we'll be listening
   struct sockaddr_storage client_addr;
@@ -114,7 +113,11 @@ int proxy(char *proxy_port)
       continue;
     }
 
-    printf("Accepted message, forking a child\n");
+    #ifdef DEBUG
+      printf("Accepted message, forking a child\n");
+    #endif
+
+    // TODO: limit number of forks
     if (!fork())
     { 
       // entire child process
@@ -127,91 +130,186 @@ int proxy(char *proxy_port)
   }
   
   // TODO: do I need to wait on all child processes?
-  return EXIT_SUCCESS;
+  return 0;
 }
 
-int handle_connection(int client_fd, struct sockaddr_storage* client_addr)
+int handle_connection(int client_fd)
 {
-  // Grab the client's IP and print (just for fun)
-  char s[INET6_ADDRSTRLEN];
-  inet_ntop(
-    client_addr->ss_family, 
-    get_in_addr((struct sockaddr *) client_addr), 
-    s, 
-    sizeof s
-  );
-  printf("server: got connection from %s\n", s);
-
-  // Need to parse/reformat and validate the client's HTTP requests
-  struct ParsedRequest* request = ParsedRequest_create();
-  void read_http_request(client_fd, request);
-
-  // Now, we need to forward that request
-  char response[MAX_BUFFER_SIZE];
-  void forward_http_request();
-
-  // The returned HTTP is directly sent to the client
-  void return_http_request(client_fd, response);
+  struct ParsedRequest* request_buffer = ParsedRequest_create();
+  int status = _try_handle_connection(client_fd, request_buffer);
 
   // Clean up the connection
-  ParsedRequest_destroy(request);
-  int err = close(client_fd);
-  if (err == -1)
+  ParsedRequest_destroy(request_buffer);
+  if (close(client_fd) == -1)
   {
     perror("Error closing client connection\n");
-    return;
   }
+  return status;
 }
 
-
-void read_http_request(int client_fd, struct ParsedRequest* request) {
-  // Read in from raw text and parse into HTTP
-  char rx_buffer[MAX_BUFFER_SIZE];
-  int bytes_rx = recv(client_fd, rx_buffer, sizeof rx_buffer, 0);
-  
-  if (ParsedRequest_parse(request, rx_buffer, sizeof rx_buffer) < 0) {
-      printf("parse failed\n");
-      return -1; // TODO: Bad request
-  }
-
-  // We only need to support GET requests
-  if (strcmp(request->method, "GET") != 0) {
-    return -1; // TODO: Not implimented
-  }
-
-  // TODO: check that GET request is formatted correctly (do I really need this?)
-
-
-  // TODO: return data necessary for forwarding (which is what?)
-
-
-  // EXAMPLE: unparsing back into HTTP, accounts for modifications to ParsedRequest
-  int request_len = ParsedRequest_totalLen(req);
-  char *return_buffer = (char *) malloc(request_len+1);
-  if (ParsedRequest_unparse(request, return_buffer, request_len) < 0) {
-    printf("unparse failed\n");
+int _try_handle_connection(int client_fd, struct ParsedRequest* request) {
+  /* Get client's requests */
+  int err = read_http_request(client_fd, request);
+  if (err == -501) {
+    #ifdef DEBUG
+      printf("Failed to parse request, sending 501 error code\n");
+    #endif
+    send(client_fd, "HTTP/1.0 501 Not Implemented\r\n\r\n", 35, 0);
+    return -1;
+  } else if (err == -400) {
+    #ifdef DEBUG
+      printf("Failed to parse request, sending 400 error code\n");
+    #endif
+    send(client_fd, "HTTP/1.0 400 Bad Request\r\n\r\n", 26, 0);
     return -1;
   }
-  return_buffer[request_len]='\0';
-  free(return_buffer);
 
-  // EXAMPLE: getting header information. Why would I need?
-  struct ParsedHeader *r = ParsedHeader_get(request, "If-Modified-Since");
-  printf("Modified value: %s\n", r->value);
+  /* Forward request to server */
+  char response[MAX_BUFFER_SIZE];
+  int response_len = forward_http_request(request, response);
+  if (err == -400) {
+    send(client_fd, "HTTP/1.0 400 Bad Request\r\n\r\n", 26, 0);
+    return -1;
+  } else if (response_len == -1) {
+    return -1;
+  }
 
+  /* Send back to the client */
+  send_text(client_fd, response, response_len);
 }
 
 
-void forward_http_request() {
-  // TODO: establish connection with server
-  // TODO: send HTTP request to server
-  // TODO: close connection with server?
-  // TODO: return the HTTP response
+int read_http_request(int client_fd, struct ParsedRequest* request) {
+  char rx_buffer[MAX_BUFFER_SIZE];
+  int bytes_rx = recv(client_fd, rx_buffer, sizeof rx_buffer, 0);
+
+  #ifdef DEBUG
+    rx_buffer[bytes_rx] = '\0';
+    printf("Request: \n\n%s\nAttempting to parse...\n", rx_buffer);
+  #endif
+
+  int err = ParsedRequest_parse(request, rx_buffer, sizeof rx_buffer);
+  if (err < 0) {
+    return -400;
+  }
+  if (strcmp(request->method, "GET") != 0) {
+    return -501;
+  }
+  return 0;
 }
 
+// Return the number of bytes that the server responded with
+int forward_http_request(struct ParsedRequest* request, char* response_buffer, int buffer_len) {
+  // If hostname does not have a port, use 80
+  char port[6];
+  if (request->port == NULL) {
+    strcpy(port, "80");
+  } else {
+    strcpy(port, request->port);
+  }
 
-void return_http_request(int client_fd) {
-  // TODO: write HTTP response to the client's fd
+  #ifdef DEBUG
+    printf("Connecting to server %s on port %s\n", port, request->host);
+  #endif
+  int server_fd = open_server_socket(request->host, port);
+  if (server_fd == -1) {
+    return -1;
+  }
+
+  // Form the HTTP request for the server (we want to close the connection after retrieval)
+  if (ParsedHeader_set(request, "Connection", "close") == -1) {
+    #ifdef DEBUG
+      printf("Failed to set 'Connection: close' header\n");
+    #endif
+    return -1;
+  }
+
+  int request_len = ParsedRequest_totalLen(request);
+  char *request_buffer = (char *) malloc(request_len+1);
+  if (ParsedRequest_unparse(request, request_buffer, request_len) == -1) {
+    #ifdef DEBUG
+      printf("Failed to unparse request, returning 400 error code\n");
+    #endif
+    free(request_buffer);
+    return -400;
+  }
+
+  // Debug request
+  #ifdef DEBUG
+    request_buffer[request_len]='\0';
+    printf("Request: \n\n%s\n", request_buffer);
+  #endif
+
+  // Send request to server
+  send_text(server_fd, request_buffer, request_len);
+  free(request_buffer);
+
+  // Receive response from server
+  int bytes_rx = recv(server_fd, response_buffer, buffer_len, 0);
+
+  #ifdef DEBUG
+    response_buffer[bytes_rx] = '\0';
+    printf("Response: \n\n%s\n", request_buffer);
+  #endif
+
+  if (close(server_fd) == -1) {
+    perror("Error closing server connection\n");
+  }
+  return bytes_rx;
+}
+
+int open_server_socket(char *server_ip, char *server_port) {
+  struct addrinfo hints, *serverinfo, *p;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  int status = getaddrinfo(server_ip, server_port, &hints, &serverinfo);
+  if (status != 0)
+  {
+    fprintf(stderr, "Error getting address info. Error code: %d\n", status);
+    return -1;
+  }
+
+  int sock_fd;
+  for (p = serverinfo; p != NULL; p = p->ai_next)
+  {
+    if ((sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      perror("client: socket");
+      continue;
+    }
+
+    if (connect(sock_fd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sock_fd);
+      perror("client: connect");
+      continue;
+    }
+    break;
+  }
+
+  if (p == NULL)
+  {
+    fprintf(stderr, "client: failed to connect\n");
+    return -1;
+  }
+  return sock_fd;
+}
+
+int send_text(int client_fd, char *data, int length) {
+  int bytes_remaining = length;
+  while (bytes_remaining > 0)
+  {
+    int bytes_sent = send(client_fd, data + (length - bytes_remaining), bytes_remaining, 0);
+    if (bytes_sent == -1)
+    {
+      perror("Error sending message");
+      return -1;
+    }
+    bytes_remaining -= bytes_sent;
+  }
+  return 0;
 }
 
 
